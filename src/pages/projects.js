c4cb9ab9085194b91a4e1b629aca831cfd52c7c1
Michael Fernandez <mikevfern@gmail.com>
2021-04-03 16:09:51 -0700

    removed blog stubs. created Projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,10 +2,39 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
-// ...GatsbyImageSharpFluid
 
-const ProjectsPage = () => {
-  return <h2>projects page</h2>
+const ProjectsPage = ({data: {allStrapiProjects: {nodes: projects}}}) => {
+  return <Layout>
+    <section className="projects-page">
+      <Projects projects={projects} title="Projects"/>
+    </section>
+  </Layout>
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        description
+        featured
+        github
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        id
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
 
 export default ProjectsPage
