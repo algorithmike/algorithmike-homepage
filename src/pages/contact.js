3a70c30d3569bf7037f67e0c75f328b8e1238e08
Michael Fernandez <mikevfern@gmail.com>
2021-04-04 19:52:03 -0700

    contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,12 +7,14 @@ const contact = () => {
       <section className="contact-page">
         <article className="contact-form">
           <h3>Contact Me</h3>
-          <div className="form-group">
-            <input type="name" className="form-control" placeholder="name"/>
-            <input type="email" className="form-control" placeholder="email"/>
-            <textarea name="messages" rows="5" placeholder="message" className="form-control"/>
-          </div>
-          <button type="submit" className="submit-btn btn">Submit Message</button>
+          <form action="https://formspree.io/f/xayayebr" method="POST">
+            <div className="form-group">
+              <input name="name" type="name" className="form-control" placeholder="name"/>
+              <input name="email" type="email" className="form-control" placeholder="email"/>
+              <textarea name="messages" rows="5" placeholder="message" className="form-control"/>
+            </div>
+            <button type="submit" className="submit-btn btn">Submit Message</button>
+          </form>
         </article>
       </section>
     </Layout>
