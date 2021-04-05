c909b38793d9d70e552f5a20fd2ef233a21a2124
Michael Fernandez <mikevfern@gmail.com>
2021-04-04 19:40:11 -0700

    contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,21 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return (
+    <Layout>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>Contact Me</h3>
+          <div className="form-group">
+            <input type="name" className="form-control" placeholder="name"/>
+            <input type="email" className="form-control" placeholder="email"/>
+            <textarea name="messages" rows="5" placeholder="message" className="form-control"/>
+          </div>
+          <button type="submit" className="submit-btn btn">Submit Message</button>
+        </article>
+      </section>
+    </Layout>
+  )
 }
 
 export default contact
