30e174bd1cbe45c3e07cdeaf987bcd6968a5e7e3
Michael Fernandez <mikevfern@gmail.com>
2021-04-16 20:26:46 -0700

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,27 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div>
-        <SocialLinks styleClass="footer-links"/>
-        <h4>Built with <a href="https://reactjs.org/" target="_blank">React</a> and <a href="https://www.gatsbyjs.com/" target="_blank">Gatsby</a></h4>
-        <p>Check out the <a href="https://github.com/algorithmike/algorithmike-homepage" target="_blank">repository</a> on Github</p>
+        <SocialLinks styleClass="footer-links" />
+        <h4>
+          Built with{" "}
+          <a href="https://reactjs.org/" target="_blank">
+            React
+          </a>{" "}
+          and{" "}
+          <a href="https://www.gatsbyjs.com/" target="_blank">
+            Gatsby
+          </a>
+        </h4>
+        <p>
+          Check out the{" "}
+          <a
+            href="https://github.com/algorithmike/algorithmike-homepage"
+            target="_blank"
+          >
+            code repository
+          </a>{" "}
+          on Github
+        </p>
       </div>
     </footer>
   )
