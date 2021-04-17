1e4e12127f2a272478643830ba2bb665692938a7
Michael Fernandez <mikevfern@gmail.com>
2021-04-16 20:25:53 -0700

    title glow animation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,11 +3,11 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
-import resume from '../assets/_MichaelFernandez-Resume-April2021.pdf'
+import resume from "../assets/_MichaelFernandez-Resume-April2021.pdf"
 
 const query = graphql`
   {
-    file(relativePath: {eq: "hero-img.png"}) {
+    file(relativePath: { eq: "hero-img.png" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid
@@ -18,22 +18,32 @@ const query = graphql`
 `
 
 const Hero = () => {
-  const {file: {childImageSharp: {fluid}}} = useStaticQuery(query);
-  return <header className="hero">
-    <div className="section-center hero-center">
-      <article className="hero-info">
-        <div>
-          <div className="underline"/>
-          <h1>Hi, I'm Mike</h1>
-          <h4>Software Engineer</h4>
-          <Link to="/contact" className="btn">Contact Me</Link>
-          <a href={resume} download><div className="btn">Download Resume</div></a>
-          <SocialLinks/>
-        </div>
-      </article>
-      <Image fluid={fluid} className="hero-img"/>
-    </div>
-  </header>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline" />
+            <h1>Hi, I'm Mike</h1>
+            <h4>Software Engineer</h4>
+            <Link to="/contact" className="btn">
+              Contact Me
+            </Link>
+            <a href={resume} download>
+              <div className="btn">Download Resume</div>
+            </a>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
 }
 
 export default Hero
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,10 @@
 import React from "react"
 
-const Title = ({title}) => {
+const Title = ({ title }) => {
   return (
     <div className="section-title">
-      <h2>{title || 'Homepage'}</h2>
-      <div className="underline"/>
+      <h2 className="glow">{title || "Homepage"}</h2>
+      <div className="underline" />
     </div>
   )
 }
