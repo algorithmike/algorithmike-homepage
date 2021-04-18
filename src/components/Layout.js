707bfaf21f2e8d3a77f83898d1a6516db2923378
Michael Fernandez <mikevfern@gmail.com>
2021-04-17 20:04:51 -0700

    metadata

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,25 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 
 const Layout = ({ children }) => {
-  const [sideBarIsOpen, setSideBarIsOpen] = React.useState(false);
-  const toggleSideBar = () => setSideBarIsOpen(!sideBarIsOpen);
+  const [sideBarIsOpen, setSideBarIsOpen] = React.useState(false)
+  const toggleSideBar = () => setSideBarIsOpen(!sideBarIsOpen)
   return (
     <>
-      <Navbar toggleSideBar={toggleSideBar}/>
-      <Sidebar isOpen={sideBarIsOpen} toggleSideBar={toggleSideBar}/>
+      <Helmet>
+        <title>Michael Fernandez | Software Engineer</title>
+        <meta
+          name="description"
+          content="Learn about some of Michael Fernandez and some of his work experience, education, and personal projects as a software engineer/developer."
+        />
+      </Helmet>
+      <Navbar toggleSideBar={toggleSideBar} />
+      <Sidebar isOpen={sideBarIsOpen} toggleSideBar={toggleSideBar} />
       {children}
-      <Footer/>
+      <Footer />
     </>
   )
 }
